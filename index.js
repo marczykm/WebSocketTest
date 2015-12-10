0293366af34c5f497f233b3b184068b6b5c6c332
Marcin Marczyk <marcin.marczyk@capgemini.com>
2015-12-10 11:32:52 +0100

    messages sending with socket.io in form

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('chat message', function(msg){
+		console.log(msg);
+	});
+	socket.on('disconnect', function(){
+		console.log('a user diconnected');
+	});
+});
+
 http.listen(3000, function() {
     console.log('listening on *:3000');
 });
